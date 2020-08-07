cab0ab4ea3189bc5b40055936595efec0af4512d
xaviguasch <xaviguasch@gmail.com>
2020-08-07 12:29:08 +0200

    added initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Fetch - Challenge
+ *
+ * GET the first comments value 'https://jsonplaceholder.typicode.com/comments/1' and log its value.
+ * POST a new comment using 'https://jsonplaceholder.typicode.com/comments' and log its value.
+ *
+ * RESTFul API Guide - https://jsonplaceholder.typicode.com/guide.html
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ */
