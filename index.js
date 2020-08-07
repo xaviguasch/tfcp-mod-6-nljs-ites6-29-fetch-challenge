063ea072b469756654a54f6a655bf90ca603aec4
xaviguasch <xaviguasch@gmail.com>
2020-08-07 12:43:01 +0200

    added solution

--- index.js
+++ index.js
@@ -7,3 +7,22 @@
  * RESTFul API Guide - https://jsonplaceholder.typicode.com/guide.html
  * Docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  */
+
+fetch('https://jsonplaceholder.typicode.com/comments/1')
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
+fetch('https://jsonplaceholder.typicode.com/comments', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'Comment 105',
+    email: 'dylansemail310@gmail.com',
+    body: 'Dopes comment in the game',
+    postId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json))
